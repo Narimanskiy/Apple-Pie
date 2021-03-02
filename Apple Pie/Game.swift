41c0a4ffdafe7211c620d7b0deaa4ac1cf7d95c7
Nariman Narimanov <Narimanov7@yandex.ru>
2021-03-02 22:05:13 +0300

    Added Game model

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Нариман on 02.03.2021.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemainig: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,11 +8,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+        "Ауди",
+        "БМВ",
+        "Бриллианс",
+        "Кадилак",
+        "Чанган",
+        "Чери",
+        "Чери Эксид",
+        "Шевроле",
+        "Крайслер",
+        "Ситроэн",
+        "Датсун",
+        "Дунфэн",
+        "ФАВ",
+        "Генезис",
+        "Хавейл",
+        "Хонда",
+        "Хёндэ",
+        "Инфинити",
+        "Ягуар",
+        "Джип",
+        "Киа",
+        "Лада",
+        "Лэнд Ровер",
+        "Лексус",
+        "Лифан",
+        "Мазда",
+        "Мерседес-Бенц",
+        "Мини",
+        "Мицубиси",
+        "Ниссан",
+        "Опель",
+        "Пежо",
+        "Порше",
+        "Рено",
+        "Шкода",
+        "Субару",
+        "Сузуки",
+        "Тойота",
+        "Фольксваген",
+        "Вольво",
+        "Зоти",
+        "УАЗ",
+        "Акура",
+        "Альфа Ромео",
+        "БИД",
+        "Додж",
+        "Грейт-Уолл",
+        "Хайма",
+        "Хавтай",
+        "Джак",
+        "Равон",
+        "Сеат",
+        "Смарт",
+        "СанЙонг",
+        "Вортекс",
+        "ЗАЗ",
+        "Богдан",
+        "ТагАЗ",
+    ]
+    var totalWins = 0
+    var totalLosses = 0
+    
+    //MARK: - Methods
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemainig: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let movesRemainig = currentGame.incorrectMovesRemainig
+        let image = "Tree\(movesRemainig < 8 ? movesRemainig : 7)"
+        treeImageView.image = UIImage(named: image)
+        scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
+    
+    //MARK: IB Actions
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 
 
 }
